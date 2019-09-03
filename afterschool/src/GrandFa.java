eae65547deff68e33ebb38b6d6aeb8129bc61dcf
jihyeon <gkawlgus01@naver.com>
2019-09-03 16:27:45 +0900

    add 객체와 클라스 이해하기

---
+++ afterschool/src/GrandFa.java
@@ -0,0 +1,16 @@
+
+public class GrandFa {
+	//정보은닉(정당한 관한을 갖고 있는 사람이 정보를 허용할 수 있게 하는 것)
+	//정보은닉 = 캡슐화
+	//정보은닉 -> 클래스 -> 캡슐화
+
+	private int nai = 80;
+	public String gabo = "상감청자";
+	protected String gahun = "모든 일에 최선을";
+	
+	public int getNai() {
+		return nai;
+	}
+	
+	
+}
---
+++ afterschool/src/MyFamily.java
@@ -0,0 +1,13 @@
+
+public class MyFamily {
+
+	public static void main(String[] args) {
+
+		GrandFa gr = new GrandFa();
+		
+		
+		System.out.println(gr.getNai());
+		
+	}
+
+}
