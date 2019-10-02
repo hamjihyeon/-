f992ae4250712e3c73f4555d8a829f91e37e0918
jihyeon <gkawlgus01@naver.com>
2019-10-02 09:06:03 +0900

    BorderLayout

--- 09.06_GUI/src/GUI5_Layout.java
+++ 09.06_GUI/src/GUI5_Layout.java
@@ -6,13 +6,23 @@ public class GUI5_Layout {
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
 
-        GridLayout gl = new GridLayout(2,3);
-        panel.setLayout(gl);
+        BorderLayout bl = new BorderLayout();
+        panel.setLayout(bl);
 
-        for(int i = 0; i<6; i++) {
-            JButton bt = new JButton("Button" + i);
-            panel.add(bt);
-        }
+
+            JButton bt0 = new JButton("Button" + 0);
+            JButton bt1 = new JButton("Button" + 1);
+            JButton bt2 = new JButton("Button" + 2);
+            JButton bt3 = new JButton("Button" + 3);
+            JButton bt4 = new JButton("Button" + 4);
+            JButton bt5 = new JButton("Button" + 5);
+            panel.add(bt0, BorderLayout.NORTH);
+            panel.add(bt1, BorderLayout.WEST);
+            panel.add(bt2, BorderLayout.CENTER);
+            panel.add(bt3, BorderLayout.EAST);
+            panel.add(bt4, BorderLayout.SOUTH);
+            panel.add(bt5, BorderLayout.NORTH);
+
 
         frame.add(panel);
 
@@ -21,3 +31,4 @@ public class GUI5_Layout {
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+}
